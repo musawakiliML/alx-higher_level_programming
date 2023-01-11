192bad4aad50bdbaa5b257a277f3bc2258ff7d98
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-01-11 23:43:50 +0000

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,23 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
-    num_args = len(sys.argv) - 1
-    if num_args != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
+if __name__ != "__main__":
+    exit()
 
-    if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    args = len(argv) - 1
+
+    if args != 3:
+        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    elif argv[2] == '+':
+        result = add(int(argv[1]), int(argv[3]))
+    elif argv[2] == '-':
+        result = sub(int(argv[1]), int(argv[3]))
+    elif argv[2] == '*':
+        result = mul(int(argv[1]), int(argv[3]))
+    elif argv[2] == '/':
+        result = div(int(argv[1]), int(argv[3]))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
+
+    print("{:s} {:s} {:s} = {:d}".format(argv[1], argv[2], argv[3], result))
