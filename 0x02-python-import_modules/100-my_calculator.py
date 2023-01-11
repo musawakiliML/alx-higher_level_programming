1451e7a8a509dfcde583844180ae14abb2b4c9ba
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-01-11 23:29:31 +0000

    update

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-
-
 if __name__ == "__main__":
-    """
-    Handle basic arithmetic operations
-    Using the functions
-    From calculator_1 module
-    """
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if len(sys.argv) - 1 != 3:
+    num_args = len(sys.argv)
+    if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    operator = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(operator.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
+        exit(1)
     a = int(sys.argv[1])
+    op = sys.argv[2]
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, operator[sys.argv[2]](a, b)))
-    sys.exit(0)
+
+    if op is '+':
+        print("{} {} {} = {}".format(a, op, b, add(a, b)))
+    elif op is '-':
+        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+    elif op is '*':
+        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+    elif op is '/':
+        print("{} {} {} = {}".format(a, op, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
