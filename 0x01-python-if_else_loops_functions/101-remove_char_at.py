c70be1b9366c2a7a9f56922a837a590f79ec4c09
Musa Adamu Wakili <musawakiliml@hotmail.com>
2023-01-21 23:25:45 +0000

    updated

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def remove_char_at_place(str_val, n):
+    if n < 0:
+        return str_val
+    count = 0
+    str_copy = ""
+    for val in str_val:
+        if count == n:
+            count += 1
+            continue
+        str_copy += str_val[count]
+        count += 1
+    return str_copy
